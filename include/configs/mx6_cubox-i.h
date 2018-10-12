024094ff149f8dc2749cd032a56d8abb2868908a
Jake Barnes <jakerodneybarnes@gmail.com>
2018-10-12 14:30:53 +1100

    Rename extrabootargs to custombootargs to avoid upgrade issues

--- include/configs/mx6_cubox-i.h
+++ include/configs/mx6_cubox-i.h
@@ -213,9 +213,9 @@
   "setenv mmcargs 'setenv mmcroot ${mmcroot};'; " \
   /* how to set bootargs */ \
   "setenv basebootargs 'console=ttymxc0,115200 video=mxcfb0:dev=hdmi,1920x1080M@60 quiet consoleblank=0 logo.nologo=1 vmalloc=320M'; " \
-  "setenv extrabootargs ''; " \
-  "setenv loadbootargs 'setenv bootargs root=${mmcroot} rootfstype=ext4 rootflags=data=writeback rootwait ro fsck.mode=force fsck.repair=yes ${basebootargs} ${extrabootargs};'; " \
-  "setenv loadusbbootargs 'setenv bootargs root=/dev/sda1 rootfstype=ext4 rootwait ${basebootargs} ${extrabootargs};'; " \
+  "setenv custombootargs ''; " \
+  "setenv loadbootargs 'setenv bootargs root=${mmcroot} rootfstype=ext4 rootflags=data=writeback rootwait ro fsck.mode=force fsck.repair=yes ${basebootargs} ${custombootargs};'; " \
+  "setenv loadusbbootargs 'setenv bootargs root=/dev/sda1 rootfstype=ext4 rootwait ${basebootargs} ${custombootargs};'; " \
   /* how to load kernel image */ \
   "setenv image zImage; " \
   "setenv bootm_size 0x10000000; " \
